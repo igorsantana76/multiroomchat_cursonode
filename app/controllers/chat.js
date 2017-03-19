76ebc0e190473e1c093f2dcda8ff5d528728c6e1
Igor Virginio <Igor Virginio>
2017-03-19 20:30:28 -0300

    59 - Recebendo e validando os dados do formulario de entrada

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,16 @@
 module.exports.initChat = function(application, req, res){
+
+    var dadosForm = req.body;
+
+    req.assert('name','Nome deve ser preenchido.').notEmpty();
+    req.assert('name','Nome deve ter entre 3 a 25 caracteres').len(3, 25);
+
+    var errors = req.validationErrors();
+
+    if(errors){
+        res.render('index', {validacao: errors});
+        return;
+    }
+
     res.render('chat');
 }
