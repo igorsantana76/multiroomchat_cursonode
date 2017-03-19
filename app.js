802d90a820ff71163c0f12630e81e4bbb4d7066b
Igor Virginio <Igor Virginio>
2017-03-19 20:38:48 -0300

    Inclusão do Socket.io

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 var app = require('./config/server');
 
-app.listen(80, function(){
+var server = app.listen(80, function(){
     console.log('Server online.');
 });
+
+require('socket.io').listen(server);
